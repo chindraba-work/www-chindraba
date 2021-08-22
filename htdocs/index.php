a66a5b13f6c0307ae9834eea7d2635e7bbb4cb2d
Ronald Lamoreaux <gitkey@chindraba.work>
2021-08-22 13:24:18 +0000

    Move favorite icons to assets
    Clean up the root directory by moving the favicons, and related images,
    to a directory in assets. Update the rendered HTML to adjust.
    
    Add a plain .ico file, somehow missed in the original copy of the site.
    
    The favicon.ico file is kept in the root directory since that's where
    everyone looks for it when linking to the site without a rendered page
    to display. All the other icons can be moved since they are only linked
    after viewing a rendered page and the client will have their full path.
    
    Dropped the now defunct file index.html
    
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

--- htdocs/index.php
+++ htdocs/index.php
@@ -45,31 +45,31 @@ print <<<EOHTML
     <meta name="viewport" content="viewport-fit=contain, width=device-width, height=device-height, initial-scale=1.0">
     <meta name="author" content="Chindraba (Ronald Lamoreaux)">
     <title>[ return || !return ] - Rabbit Hole Reports Blog (FEWDR Final Project: Ronald Lamoreaux)</title>
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/apple-touch-icon-60x60.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-    <link rel="icon" type="image/png" href="/favicon-196x196.png" sizes="196x196" />
-    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="/favicon-128.png" sizes="128x128" />
-    <link rel="shortcut icon" type="image/png" href="/favicon-196x196.png" sizes="196x196" />
-    <link rel="shortcut icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-    <link rel="shortcut icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-    <link rel="shortcut icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-    <link rel="shortcut icon" type="image/png" href="/favicon-128.png" sizes="128x128" />
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/favicons/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/favicons/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/favicons/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/favicons/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/assets/favicons/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/assets/favicons/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/assets/favicons/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/assets/favicons/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="/assets/favicons/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="/assets/favicons/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="/assets/favicons/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/assets/favicons/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="/assets/favicons/favicon-128.png" sizes="128x128" />
+    <link rel="shortcut icon" type="image/png" href="/assets/favicons/favicon-196x196.png" sizes="196x196" />
+    <link rel="shortcut icon" type="image/png" href="/assets/favicons/favicon-96x96.png" sizes="96x96" />
+    <link rel="shortcut icon" type="image/png" href="/assets/favicons/favicon-32x32.png" sizes="32x32" />
+    <link rel="shortcut icon" type="image/png" href="/assets/favicons/favicon-16x16.png" sizes="16x16" />
+    <link rel="shortcut icon" type="image/png" href="/assets/favicons/favicon-128.png" sizes="128x128" />
     <meta name="application-name" content="Chindraba"/>
     <meta name="msapplication-TileColor" content="#12313B" />
-    <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
-    <meta name="msapplication-square70x70logo" content="/mstile-70x70.png" />
-    <meta name="msapplication-square150x150logo" content="/mstile-150x150.png" />
-    <meta name="msapplication-wide310x150logo" content="/mstile-310x150.png" />
-    <meta name="msapplication-square310x310logo" content="/mstile-310x310.png" />
+    <meta name="msapplication-TileImage" content="/assets/favicons/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="/assets/favicons/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="/assets/favicons/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="/assets/favicons/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="/assets/favicons/mstile-310x310.png" />
     <link href="/css/common.css" rel="stylesheet" type="text/css">
 </head>
 <body>
