e89589c8a44b61280ff969538ffb205693b1ff0f
Ronald Lamoreaux <gitkey@chindraba.work>
2021-09-04 03:13:09 +0000

    Add possible content - hello world
    While unpacking from the move I found this piece. While it might have
    no purpose, I figured I'd add it here for possible use as a blog entry.
    
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

---
+++ htdocs/hello-world.php
@@ -0,0 +1,63 @@
+<?php
+// SPDX-License-Identifier: MIT
+
+/***********************************************************************
+*                                                                      *
+*  This file is part of collection of scripts and files used to create *
+*  the website for Chindraba.                                          *
+*                                                                      *
+*  Copyright © 2020 - 2021  Chindraba (Ronald Lamoreaux)               *
+*                    <website@chindraba.work>                          *
+*  - All Rights Reserved                                               *
+*                                                                      *
+*  Permission is hereby granted, free of charge, to any person         *
+*  obtaining a copy of this software and associated documentation      *
+*  files (the "Software"), to deal in the Software without             *
+*  restriction, including without limitation the rights to use, copy,  *
+*  modify, merge, publish, distribute, sublicense, and/or sell copies  *
+*  of the Software, and to permit persons to whom the Software is      *
+*  furnished to do so, subject to the following conditions:            *
+*                                                                      *
+*  The above copyright notice and this permission notice shall be      *
+*  included in all copies or substantial portions of the Software.     *
+*                                                                      *
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,     *
+*  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF  *
+*  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND               *
+*  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS *
+*  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN  *
+*  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN   *
+*  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    *
+*  SOFTWARE.                                                           *
+*                                                                      *
+************************************************************************/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <p>There is a long tradition in programming languages that your first program in any language should simply say "Hello World!" and do nothing else.</p>
+    <p>There is actually a very good practical reason for this habit. <code>Hello World</code> is the Simplest possible program you can write that you can prove works. <code>Hello World</code> programs are use to help you figure out the mechanics of the progamming environment &mdash; how the program is written, what special steps you have to do to make the code run, and how it works.</p>
+    <p> There is no point in making a complicated program until you know you can get the code to pop up and say "hi."</p>
+    <pre>
+        &lt;!DOCTYPE html PUBLIC "-//W3C/DTD XHTML 1.0 Strict/EN" "http://www.w3c.org/TR/xhtml1/DTD/xhtml1-strict/dtd"&gt;
+        &lt;html lang="en" dir="ltf" xmln"http://www.w3x.org/1999/xhtml"&gt;
+            &lt;head&gt;
+                &lt;meta httpd-equiv="content-type" content="test/xml; charset=utf-8" /&gt;
+                &lt;title&gt;Hello World&lt;/title&gt;
+                &lt;script type="text/javascript"&gt;
+                    //&lt;![CDATA[
+                        // Hello World!
+                        alert("Hello World!");
+                    //]]&gt;
+                &lt;/script&gt;
+            &lt;/head&gt;
+            &lt;body&gt;
+            &lt;/body&gt;
+        &lt;/html&gt;
+    </pre>
+</body>
+</html>
+<?php
+// vim: set syntax=php ts=4 sw=4 sts=4 et sr:
